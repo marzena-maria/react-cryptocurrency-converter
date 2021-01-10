a5dbf549455c46bcfd8f26faa58e48825143dc0f
Marzena Studniarz <marzena.kupiec@gmail.com>
2021-01-10 20:35:15 +0100

    Updates

---
+++ precache-manifest.071f5ff4ad6276690725a86d26b9440c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d1769840bda56820e025c6c2cdb321",
+    "url": "/react-cryptocurrency-converter/index.html"
+  },
+  {
+    "revision": "d134036d60e1b2a75cad",
+    "url": "/react-cryptocurrency-converter/static/css/main.8f0a66e9.chunk.css"
+  },
+  {
+    "revision": "42b1034a685f817576d9",
+    "url": "/react-cryptocurrency-converter/static/js/2.98c878d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cryptocurrency-converter/static/js/2.98c878d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d134036d60e1b2a75cad",
+    "url": "/react-cryptocurrency-converter/static/js/main.154fc981.chunk.js"
+  },
+  {
+    "revision": "fee598e0ff99ab354c2a",
+    "url": "/react-cryptocurrency-converter/static/js/runtime-main.041ab4fd.js"
+  }
+]);
